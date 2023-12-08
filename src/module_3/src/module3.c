e9d39a6ae254e34d92c6f2053607a636cbc4b6cc
Berkay <47042804+berkayozkarhan@users.noreply.github.com>
2023-12-08 10:43:21 +0300

    Create module3.c

---
+++ src/module_3/src/module3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "module_3/module_3.hh"
+
+void module_3_print_hello(void)
+{
+    printf("Hello from module_1!\n");
+}
+
+int module_3_multiply_numbers(int a, int b)
+{
+    return a * b;
+}
